28287b5f467c71cb5a29125145c1dac1dd86d857
loha81 <github@nospam.andreasloh.de>
2016-03-19 20:49:06 +0100

    Added new Extension TobiiEyeTracking

---
+++ Extensions/ProjectWerner.TobiiEyeTracking/Views/MainView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Controls;
+using ProjectWerner.Contracts.Extensions;
+
+namespace ProjectWerner.TobiiEyeTracking.Views
+{
+    [AppExtensionMetadataAttribute(Name = "B")]
+    [Export(typeof(IAppExtension))]
+    public partial class MainView : IAppExtension
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        public UserControl AppUserControl { get { return this; } }
+    }
+}
