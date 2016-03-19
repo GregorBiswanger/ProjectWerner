bf57ac3e9a07f0b4bc11525c94aeb91809df8a73
loha81 <github@nospam.andreasloh.de>
2016-03-19 20:38:53 +0100

    Added IEyeTracking contract

---
+++ ProjectWerner.Contracts/API/IEyeTracking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows;
+
+namespace ProjectWerner.Contracts.API
+{
+    [InheritedExport]
+    public interface IEyeTracking
+    {
+        event Action<Point> EyePositionUpdate;
+        event Action LeftEyeBlinked;
+        event Action RightEyeBlinked;
+    }
+}
