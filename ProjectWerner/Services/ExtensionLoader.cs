6361dfcd1f2410db99046b88a646024c2b6dc24c
Matthias Drescher <kingxelor@gamil.com>
2016-03-19 11:33:01 +0100

    refactored mainWindow towards MVVM

---
+++ ProjectWerner/Services/ExtensionLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+using ProjectWerner.Contracts.Extensions;
+
+namespace ProjectWerner.Services
+{
+	internal class ExtensionLoader
+	{
+		[Import]
+		private IAppExtension appExtension;
+
+		public ExtensionLoader()
+		{
+			App.CompositionContainer.ComposeParts(this);
+		}
+
+		public IAppExtension GetFaceToSpeachExtension()
+		{
+			return appExtension;
+		}
+	}
+}
