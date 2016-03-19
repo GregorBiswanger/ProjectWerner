183fda2ca21e534385f3f5aee3604836b39f9d19
Alexander Horn <alexander.horn@bytepassion.de>
2016-03-19 11:39:07 +0100

    added first placeholder extension

--- ProjectWerner.Face2Speech/Views/Keyboard.xaml.cs
+++ ProjectWerner.Face2Speech/Views/Keyboard.xaml.cs
@@ -1,19 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ProjectWerner.Contracts.API;
+﻿using System.Windows.Controls;
 using ProjectWerner.Contracts.Extensions;
 
 namespace ProjectWerner.Face2Speech.Views
