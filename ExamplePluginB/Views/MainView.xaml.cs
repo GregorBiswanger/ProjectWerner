d4323228cb574c75e99612d065188f2f4cada3bf
Alexander Horn <alexander.horn@bytepassion.de>
2016-03-19 12:49:46 +0100

    added second example plugin

---
+++ ExamplePluginB/Views/MainView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using ProjectWerner.Contracts.Extensions;
+
+namespace ProjectWerner.ExampleExenstionB.Views
+{
+    /// <summary>
+    /// Interaktionslogik für MainView.xaml
+    /// </summary>
+    public partial class MainView : IAppExtension
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        public UserControl AppUserControl { get { return this; } }
+    }
+}
