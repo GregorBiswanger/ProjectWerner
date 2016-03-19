f4927ee73304f88c7cbf22d1ea32155fcb533abd
Matthias Drescher <kingxelor@gamil.com>
2016-03-19 13:06:12 +0100

    selection moves every 2 seconds

--- ProjectWerner/ViewModels/MainWindow/MainWindowViewModel.cs
+++ ProjectWerner/ViewModels/MainWindow/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Windows.Input;
+using System.Windows.Threading;
 using ProjectWerner.Dto;
 using ProjectWerner.MvvmHelper.Commands;
 using ProjectWerner.MvvmHelper.ViewModelBase;
@@ -13,9 +15,21 @@ using ProjectWerner.Views;
 namespace ProjectWerner.ViewModels.MainWindow
 {
 	internal class MainWindowViewModel : ViewModel, IMainWindowViewModel
-	{		
+	{
+		private DispatcherTimer selectionTimer;
+		private int selectedItem;
+
 		public MainWindowViewModel(IExtensionLoader extensionLoader)
 		{
+			selectedItem = 0;
+			selectionTimer = new DispatcherTimer
+			{
+				Interval = TimeSpan.FromSeconds(2),
+				IsEnabled = true
+			};
+
+			selectionTimer.Tick += OnSeletionTimerTick;
+
 			Extensions = new ObservableCollection<ExtensionDataSet>(extensionLoader.GetExtensions());
 			Extensions.First().IsSelected = true;
 
@@ -32,6 +46,18 @@ namespace ProjectWerner.ViewModels.MainWindow
 					extensionWindow.Show();
 				}	
 			);
+		}
+
+		private void OnSeletionTimerTick(object sender, EventArgs eventArgs)
+		{
+			Extensions[selectedItem].IsSelected = false;
+
+			selectedItem++;
+
+			if (selectedItem == Extensions.Count)
+				selectedItem = 0;
+
+			Extensions[selectedItem].IsSelected = true;
 		}
 
 		public ICommand ExecuteExtension { get; }
