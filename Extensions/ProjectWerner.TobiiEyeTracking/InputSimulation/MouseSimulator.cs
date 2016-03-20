39ff03f7981d6c512a384511908efb3e6b33ff37
loha81 <github@nospam.andreasloh.de>
2016-03-20 14:38:58 +0100

    Added MouseSimulator (uses InputSimulator NuGet package)

---
+++ Extensions/ProjectWerner.TobiiEyeTracking/InputSimulation/MouseSimulator.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using WindowsInput;
+
+namespace ProjectWerner.TobiiEyeTracking.InputSimulation
+{
+    class MouseSimulator
+    {
+        // todo: use native methods to remove NuGet dependency on InputSimulator
+        readonly InputSimulator _inputSimulator = new InputSimulator();
+
+        public void MoveMouseTo(Point point)
+        {
+            SetCursorPos((int)point.X, (int)point.Y);
+        }
+
+        public void MouseLeftClick()
+        {
+            _inputSimulator.Mouse.LeftButtonClick();
+        }
+
+        public void MouseRightClick()
+        {
+            _inputSimulator.Mouse.RightButtonClick();
+        }
+
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int x, int y);
+    }
+}
